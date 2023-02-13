fd652a0e69c2927f2a5426ccf9b77f93a7ea6e14
Nick <nholden10@gmail.com>
2023-02-12 22:50:01 -0800

    complete UI, progressing on API

---
+++ pages/api/generate.js
@@ -0,0 +1,32 @@
+const generateAction = async function (req, res) {
+  console.log("Received request.");
+
+  const input = JSON.parse(req.body).promptText;
+
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/sd-dreambooth-library/avator-generator-2`,
+    {
+      headers: {
+        Authroization: `Bearer ${process.env.HF_AUTH_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        inputs: input,
+      }),
+    }
+  );
+
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    res.status(200).json({ image: buffer });
+  } else if (response.status === 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,73 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+
+import { useState } from "react";
 
 const Home = () => {
+  const [promptText, setPromptText] = useState("");
+  const [img, setImg] = useState("");
+
+  function handleChange(event) {
+    setPromptText(event.target.value);
+  }
+
+  async function onGenerate() {
+    console.log("Generating image...");
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+      body: JSON.stringify({ promptText }),
+    });
+    const data = await response.json();
+
+    if (response.status === 503) {
+      console.log("Model is still loading...");
+      return;
+    }
+
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      return;
+    }
+
+    setImg(data.image);
+  }
+
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>Nick AI Avatar Generator</title>
       </Head>
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>your generator one-liner</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>description of your generator</h2>
-          </div>
+      <div className="main-container">
+        <div className="title-container">
+          <h1 className="title">Welcome to the Nick avator generator.</h1>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        <div className="content-container">
+          <h2 className="description">
+            Make a Nick avator in any style using Stable Diffusion! Enter a
+            prompt in the textbox and hit generate. Ensure you have the word
+            "nickho" in the prompt as that is the text that the model learned to
+            associate with photos of Nick.
+          </h2>
+          <div className="prompt-container">
+            <label className="prompt-label">Prompt:</label>
+            <input
+              placeholder="Ex) nickho, comic animation, Stan Lee, high resolution"
+              className="input-prompt"
+              type="text"
+              id="prompt"
+              name="prompt"
+              value={promptText}
+              onChange={handleChange}
+            />
           </div>
-        </a>
+          <button className="button" onClick={onGenerate}>
+            Generate!
+          </button>
+        </div>
       </div>
     </div>
   );
